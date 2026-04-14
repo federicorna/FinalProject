b66da70e33b05d60973f603b089a74883cc0cb5b
Federico Corna <federicorna@icloud.com>
2026-04-14 20:30:14 +0200

    codice movimento copiato

---
+++ Assets/_Project/Script/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float _moveSpeed = 5;
+    [SerializeField] private float _mouseSensitivity = 2f;
+    [SerializeField] private float _jumpHeight = 2;
+
+    private Rigidbody _rb;
+    private GroundChecker _groundChecker;
+
+    private float _v, _h;
+    private float _mouseX;
+    private bool _isJumping;
+
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _groundChecker = GetComponentInChildren<GroundChecker>();
+    }
+
+    void Update()
+    {
+        _h = Input.GetAxisRaw("Horizontal");
+        _v = Input.GetAxisRaw("Vertical");
+
+        _mouseX += Input.GetAxis("Mouse X") * _mouseSensitivity;
+
+        _isJumping = Input.GetButtonDown("Jump");
+
+        if (_isJumping && _groundChecker.IsGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Quaternion playerRotation = Quaternion.Euler(0, _mouseX, 0);
+        _rb.MoveRotation(playerRotation);
+
+        Vector3 moveDir = (transform.forward * _v) + (transform.right * _h);
+
+        Vector3 targetPosition = _rb.position + moveDir.normalized * (_moveSpeed * Time.fixedDeltaTime);
+        _rb.MovePosition(targetPosition);
+
+    }
+
+    private void Jump()
+    {
+        _rb.AddForce(Vector3.up * Mathf.Sqrt(_jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        _isJumping = false;
+    }
+
+}
